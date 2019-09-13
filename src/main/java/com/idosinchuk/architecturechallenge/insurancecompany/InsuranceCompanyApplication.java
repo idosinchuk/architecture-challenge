3f6e49b5653a8e75f7e8cafb337a84f379dea9f9
idosinchuk <idosinchuk@host.docker.internal>
2019-09-13 09:58:18 +0200

    Configured zuul

--- src/main/java/com/idosinchuk/architecturechallenge/insurancecompany/InsuranceCompanyApplication.java
+++ src/main/java/com/idosinchuk/architecturechallenge/insurancecompany/InsuranceCompanyApplication.java
@@ -3,7 +3,7 @@ package com.idosinchuk.architecturechallenge.insurancecompany;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -20,7 +20,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-@EnableEurekaClient // Enable eureka client. It inherits from @EnableDiscoveryClient.
+@EnableDiscoveryClient
 @SpringBootApplication
 public class InsuranceCompanyApplication {
 
